aadebd3e641c705a61cb27de95997d0a2a00f5f2
praveenkrishnan <praveen.krishnan@tismotech.com>
2025-01-15 17:15:47 +0530

    updated ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       key : 'key.pem',
       user : 'ubuntu',
       host : '13.201.166.147',
-      ref  : 'origin/main',
+      ref  : 'origin/master',
       repo : 'git@github.com:Praveen007-K/test-next.js_deployment.git',
       path : '/home/ubuntu',
       'pre-deploy-local': '',
